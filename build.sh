49e53d6140e0c780fb68a22b133e3f560c51d92a
LucasBorges-Santos <lucas.borges.1303@gmail.com>
2021-08-18 15:16:43 -0300

    [UPD] Adding another dependences

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 sudo apt install gcc build-essential python3-dev \
     libxslt1-dev libldap2-dev \
     libzip-dev libssl-dev libxml2-dev \
-    libpq-dev
+    libxmlsec1-dev libxmlsec1-openssl libpq-dev \
+    swig
